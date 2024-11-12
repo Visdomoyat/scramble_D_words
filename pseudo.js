a32c94cdafd8dd84c3eb987c4d240e9b4c1a5a51
User Name <user@email.com>
2024-11-12 11:03:37 -0500

    project planning

---
+++ pseudo.js
@@ -0,0 +1,29 @@
+// PROJECT PLANNING
+// user Story: Word Scramble game
+// - As a user I want to see a landing page when I arrive a website
+// - As a user I want to be able to put my name in the game, so it becomes personal to me
+//  - As a user I want to be able to arrange the dysfunctional letters either by typing or by clicking and draging with my mouse
+//  - As a user I want to be able to see my score so as to know if I passed or failed
+//  - As a user I want to be aware of the pass mark per round so that I know how much effort to put into that round
+//  - As a user I want to know how much time  I have to finsh a round.
+//  - As a user I want to be able to shuffle the dysfunctional letters at least once so that i have an hint of the correct word
+//  - As a user I want to be able to play the same round again
+
+// Pseudo
+// 1. Define any variable used to track the state of the game:
+//  - player name
+//  - square boxes
+//  - result message : display (pass/fail)
+//  -  *if failed, try agian / quit*
+//  - score
+//  - round (round 1)
+// 2. Define the required constants:
+//  - shuffle letters(buttons)
+//  - dysfunctional letters
+//  - click on each letters(buttons) and drag / type letters
+//  - handling the random letters to be displayed (DOM)
+//  - square boxes
+// 3. compare player score with the pass score and check if pass or fail
+//  - if failed display a button (tryagin or quit)
+//  - if passed display a message nicely done!!! and *move to the next round*
+
